bccb3b03517ac29097e6bf16a089c3e00a4a65eb
Achilleas Naoumidis <anaumidis@gmail.com>
2014-11-20 18:49:20 +0200

    Refactor some code and fix conflicts after merging

--- bargainhunterandroid/src/main/java/com/bargainhunter/bargainhunterandroid/MainActivity.java
+++ bargainhunterandroid/src/main/java/com/bargainhunter/bargainhunterandroid/MainActivity.java
@@ -13,9 +13,9 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks,
-        OfferInfoFragment.OnFragmentInteractionListener,
-        StoreInfoFragment.OnFragmentInteractionListener{
-        OfferInfoFragment.OnFragmentInteractionListener, StoreListFragment.OnFragmentInteractionListener {
+        StoreListFragment.OnFragmentInteractionListener,
+        StoreInfoFragment.OnFragmentInteractionListener,
+        OfferInfoFragment.OnFragmentInteractionListener {
 
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
@@ -51,9 +51,7 @@ public class MainActivity extends ActionBarActivity
 
         // Change to Map fragment.
         Fragment fragment = StoreListFragment.newInstance(sectionNumber);
-//        Fragment fragment = OfferInfoFragment.newInstance(sectionNumber, "1");
-        // Change to Map fragment
-        Fragment fragment = StoreInfoFragment.newInstance(sectionNumber, "1");
+//        Fragment fragment = StoreInfoFragment.newInstance(sectionNumber, "1");
 //        Fragment fragment = OfferInfoFragment.newInstance(sectionNumber, "1");
 //        Fragment fragment =  MapFragment.newInstance(sectionNumber);
 
@@ -95,7 +93,6 @@ public class MainActivity extends ActionBarActivity
         actionBar.setTitle(mTitle);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         if (!mNavigationDrawerFragment.isDrawerOpen()) {
@@ -124,19 +121,16 @@ public class MainActivity extends ActionBarActivity
         return super.onOptionsItemSelected(item);
     }
 
-
-
-    @Override
-    public void onOfferInfoFragmentInteraction(Uri uri) {
-    }
-
     @Override
     public void onStoreListFragmentInteraction(String id) {
-
     }
 
     @Override
     public void onStoreInfoFragmentInteraction(Uri uri) {
+    }
+
+    @Override
+    public void onOfferInfoFragmentInteraction(Uri uri) {
     }
 
 //    /**
