b51f2cc9b6ee70130c1239f908031c6b5bc1fdd4
Achilleas Naoumidis <anaumidis@gmail.com>
2014-11-28 08:44:23 +0200

    Refactor some code and fix conflicts after merging

--- bargainhunterandroid/src/main/java/com/bargainhunter/bargainhunterandroid/controllers/adapters/OfferListController.java
+++ bargainhunterandroid/src/main/java/com/bargainhunter/bargainhunterandroid/controllers/adapters/OfferListController.java
@@ -2,17 +2,10 @@
 package com.bargainhunter.bargainhunterandroid.controllers.adapters;
 
 
-import java.util.List;
-
 import android.app.Activity;
-import android.widget.Toast;
-import retrofit.Callback;
-import retrofit.RestAdapter;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
-
-import com.bargainhunter.bargainhunterandroid.DAOs.OfferAPI;
 import com.bargainhunter.bargainhunterandroid.models.entities.Offer;
+
+import java.util.List;
 
 
 
@@ -24,28 +17,29 @@ public class OfferListController {
     List<Offer> offerList = null;
 
     public List<Offer> requestData(String mEndpoint, final Activity activity) {
-        RestAdapter adapter = new RestAdapter.Builder()
-                .setEndpoint(mEndpoint)
-                .build();
-
-        //implement the api interface
-        OfferAPI api = adapter.create(OfferAPI.class);
-
-        //connect to server and user getOffer.
-        api.getOffers(new Callback<List<Offer>>() {
-
-            //Here i can save my data if the connection was successful.
-            @Override
-            public void success(List<Offer> offers, Response response) {offerList = offers;}
-
-            //Here i can handle the Retrofit error. Connection unsuccessful.
-            @Override
-            public void failure(RetrofitError error) {
-                offerList = null;
-                Toast.makeText(activity, error.getMessage(), Toast.LENGTH_LONG).show();
-            }
-
-        });
+//        RestAdapter adapter = new RestAdapter.Builder()
+//                .setEndpoint(mEndpoint)
+//                .build();
+//
+//        //implement the api interface
+//        OfferAPI api = adapter.create(OfferAPI.class);
+//
+//        //connect to server and user getOffer.
+//        api.getOffers(new Callback<List<Offer>>() {
+//
+//            //Here i can save my data if the connection was successful.
+//            @Override
+//            public void success(List<Offer> offers, Response response) {offerList = offers;}
+//
+//            //Here i can handle the Retrofit error. Connection unsuccessful.
+//            @Override
+//            public void failure(RetrofitError error) {
+//                offerList = null;
+//                Toast.makeText(activity, error.getMessage(), Toast.LENGTH_LONG).show();
+//            }
+//
+//        });
+//
         return offerList;
     }
 }
