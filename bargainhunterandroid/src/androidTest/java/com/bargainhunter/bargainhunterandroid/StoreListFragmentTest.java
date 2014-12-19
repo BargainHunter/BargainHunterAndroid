4185a9da8f0fe5f00d3fd16a3826cbeb3dd112ef
Vasilis <vmirisas@gmail.com>
2014-12-19 17:20:59 +0200

    Android GUI Testing
    Test on StoreListFragment
    Test on OffersListFragment
    Test on MapFragment
    
    Test opens the application and selects "Stores" from the DrawerLayout
    Test opens the application and selects "Offers" from the DrawerLayout
    Test opens the application and selects "Map" from the DrawerLayout
    Multiple tests on "Stores" list and it's contents

--- bargainhunterandroid/src/androidTest/java/com/bargainhunter/bargainhunterandroid/StoreListFragmentTest.java
+++ bargainhunterandroid/src/androidTest/java/com/bargainhunter/bargainhunterandroid/StoreListFragmentTest.java
@@ -55,4 +55,181 @@ public class StoreListFragmentTest extends ActivityInstrumentationTestCase2<Main
         solo.clickOnImageButton(0);
         solo.clickOnText("Offers");
     }
+
+    public void testStoreListFirstStore() throws Exception {
+        solo.assertCurrentActivity("check on first activity", MainActivity.class);
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Stores");
+        solo.clickInList(1);
+    }
+
+    public void testStoreListFirstAndSecondStore() throws Exception {
+        solo.assertCurrentActivity("check on first activity", MainActivity.class);
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Stores");
+        solo.clickInList(1);
+        solo.goBack();
+        solo.clickInList(2);
+    }
+
+    public void testStoreListFirstStoreAndFirstOffer() throws Exception {
+        solo.assertCurrentActivity("check on first activity", MainActivity.class);
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Stores");
+        solo.clickInList(1);
+        solo.clickInList(1);
+    }
+
+    public void testStoreListFirstStoreAndSecondOffer() throws Exception {
+        solo.assertCurrentActivity("check on first activity", MainActivity.class);
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Stores");
+        solo.clickInList(1);
+        solo.clickInList(2);
+    }
+
+    public void testStoreListSecondStoreAndFirstOffer() throws Exception {
+        solo.assertCurrentActivity("check on first activity", MainActivity.class);
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Stores");
+        solo.clickInList(2);
+        solo.clickInList(1);
+    }
+
+    public void testStoreListSecondStoreAndSecondOffer() throws Exception {
+        solo.assertCurrentActivity("check on first activity", MainActivity.class);
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Stores");
+        solo.clickInList(2);
+        solo.clickInList(2);
+    }
+
+    public void testStoreListThirdStoreAndFirstOffer() throws Exception {
+        solo.assertCurrentActivity("check on first activity", MainActivity.class);
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Stores");
+        solo.clickInList(3);
+        solo.clickInList(1);
+    }
+
+    public void testStoreListThirdStoreAndSecondOffer() throws Exception {
+        solo.assertCurrentActivity("check on first activity", MainActivity.class);
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Stores");
+        solo.clickInList(3);
+        solo.clickInList(2);
+    }
+
+    public void testStoreListFourthStoreAndFirstOffer() throws Exception {
+        solo.assertCurrentActivity("check on first activity", MainActivity.class);
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Stores");
+        solo.clickInList(4);
+        solo.clickInList(1);
+    }
+
+    public void testStoreListFourthStoreAndSecondOffer() throws Exception {
+        solo.assertCurrentActivity("check on first activity", MainActivity.class);
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Stores");
+        solo.clickInList(4);
+        solo.clickInList(2);
+    }
+
+    public void testStoreListFifthStoreAndFirstOffer() throws Exception {
+        solo.assertCurrentActivity("check on first activity", MainActivity.class);
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Stores");
+        solo.clickInList(5);
+        solo.clickInList(1);
+    }
+
+    public void testStoreListFifthStoreAndSecondOffer() throws Exception {
+        solo.assertCurrentActivity("check on first activity", MainActivity.class);
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Stores");
+        solo.clickInList(5);
+        solo.clickInList(2);
+    }
+
+    public void testStoreListSixthStoreAndFirstOffer() throws Exception {
+        solo.assertCurrentActivity("check on first activity", MainActivity.class);
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Stores");
+        solo.clickInList(6);
+        solo.clickInList(1);
+    }
+
+    public void testStoreListSixthStoreAndSecondOffer() throws Exception {
+        solo.assertCurrentActivity("check on first activity", MainActivity.class);
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Stores");
+        solo.clickInList(6);
+        solo.clickInList(2);
+    }
+
+        public void testStoreListFirstStoreOffers() throws Exception {
+        solo.assertCurrentActivity("check on first activity", MainActivity.class);
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Stores");
+        solo.clickInList(1);
+        solo.clickInList(1);
+        solo.goBack();
+        solo.clickInList(2);
+    }
+
+    public void testStoreListSecondStoreOffers() throws Exception {
+        solo.assertCurrentActivity("check on first activity", MainActivity.class);
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Stores");
+        solo.clickInList(2);
+        solo.clickInList(1);
+        solo.goBack();
+        solo.clickInList(2);
+    }
+
+    public void testStoreListThirdStoreOffers() throws Exception {
+        solo.assertCurrentActivity("check on first activity", MainActivity.class);
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Stores");
+        solo.clickInList(3);
+        solo.clickInList(1);
+        solo.goBack();
+        solo.clickInList(2);
+    }
+
+    public void testStoreListFourthStoreOffers() throws Exception {
+        solo.assertCurrentActivity("check on first activity", MainActivity.class);
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Stores");
+        solo.clickInList(4);
+        solo.clickInList(1);
+        solo.goBack();
+        solo.clickInList(2);
+    }
+
+    public void testStoreListFifthStoreOffers() throws Exception {
+        solo.assertCurrentActivity("check on first activity", MainActivity.class);
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Stores");
+        solo.clickInList(5);
+        solo.clickInList(1);
+        solo.goBack();
+        solo.clickInList(2);
+    }
+
+    public void testStoreListSixthStoreOffers() throws Exception {
+        solo.assertCurrentActivity("check on first activity", MainActivity.class);
+        solo.clickOnImageButton(0);
+        solo.clickOnText("Stores");
+        solo.clickInList(6);
+        solo.clickInList(1);
+        solo.goBack();
+        solo.clickInList(2);
+    }
+
+
+
+
+
 }
