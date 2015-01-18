576a6a8ab853ccde83baf7d200bc6da0e201830d
Achilleas Naoumidis <anaumidis@gmail.com>
2015-01-18 09:04:45 +0200

    Update REST API

--- bargainhunterandroid/src/main/java/com/bargainhunter/bargainhunterandroid/restAPIs/ISearchAPI.java
+++ bargainhunterandroid/src/main/java/com/bargainhunter/bargainhunterandroid/restAPIs/ISearchAPI.java
@@ -6,8 +6,8 @@ import retrofit.http.GET;
 import retrofit.http.Query;
 
 public interface ISearchAPI {
-    //GET method for ENDPOINT/search_in_radius?latitude={}&longitude={}&radius={} (Asynchronous execution)
-    @GET("/search_in_radius")
+    //GET method for ENDPOINT/search/in_radius?latitude={}&longitude={}&radius={} (Asynchronous execution)
+    @GET("/search/in_radius")
     public void getAllBranchesWithStoresAndOffersInRadius(
             @Query("latitude") Double latitude,
             @Query("longitude") Double longitude,
